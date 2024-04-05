aa32e89ba736fcf97c33da493c87fe02dc6b69a1
abhashkumar <abhashmaddi@hotmail>
2024-04-05 10:42:52 +0530

    Adding particles support

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../Styles/Summary.css'; // Import CSS for summary animation
+import ParticleBackground from './helper/ParticleBackground';
 
 const Summary: React.FC = () => {
   return (
     <section id="summary" className="summary">
+      <ParticleBackground />
       <h2>Summary</h2>
       <p>
         Accomplished Senior Software Engineer with over 6 years of experience in full-stack development, specializing in .NET technologies, React JS, TypeScript, and AWS cloud solutions. Proven track record in system design, microservices architecture, and team leadership. Adept at migrating legacy applications to modern frameworks and driving collaborative efforts to deliver high-quality solutions.
---
+++ src/components/helper/ParticleBackground.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type ISourceOptions,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+
+import './Styles/Particle.css';
+
+const ParticleBackground = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fullScreen:false,
+      fpsLimit: 60,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#000000",
+        },
+        links: {
+          color: "#000000",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "out",
+          },
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 200,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        options={options}
+        className="particle-position"
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default ParticleBackground;
